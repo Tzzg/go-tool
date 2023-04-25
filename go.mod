e91df1943ad5883907b6c34fb9c6d0b2fe3784e9
张志国 <93757453@qq.com>
2023-04-25 09:37:29 +0800

    feat: add 常用

--- go.mod
+++ go.mod
@@ -3,3 +3,26 @@ module github.com/Tzzg/go-tool
 go 1.18
 
 require github.com/google/uuid v1.3.0
+
+require (
+	github.com/bits-and-blooms/bitset v1.7.0 // indirect
+	github.com/bits-and-blooms/bloom/v3 v3.3.1 // indirect
+	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang-module/carbon/v2 v2.2.3 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/seiflotfy/cuckoofilter v0.0.0-20220411075957-e3b120b3f5fb // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.15.0 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"github.com/Tzzg/go-tool/data"
 	"github.com/Tzzg/go-tool/worker_pool"
+	"github.com/bits-and-blooms/bloom/v3"
+	"github.com/golang-module/carbon/v2"
+	cuckoo "github.com/seiflotfy/cuckoofilter"
+	"github.com/spf13/viper"
 	"log"
 	"time"
 )
@@ -25,4 +29,48 @@ func main() {
 	}
 	log.Println("WaitAndClose")
 	wp.WaitAndClose()
+
+	// 时间处理
+	//  go get -u github.com/golang-module/carbon/v2
+	log.Println(carbon.Now())
+
+	// config 配置
+	// go get github.com/spf13/viper
+	viper.Set("KEY", "scsds")
+	log.Println(viper.GetString("KEY"))
+
+	// 布隆过滤器
+	// go get -u github.com/bits-and-blooms/bloom/v3
+	filter := bloom.NewWithEstimates(1000000, 0.01) // 构建能够接收 100 万个元素且误报率为 1% 的布隆过滤器
+	filter.Add([]byte("Love"))
+	log.Println(filter.Test([]byte("Love")))
+	log.Println(filter.Test([]byte("Lov1e")))
+
+	// 布谷鸟过滤器 提供了动态添加和删除项目的灵活性
+	// go get -u github.com/seiflotfy/cuckoofilter
+	cf := cuckoo.NewFilter(1000)
+	cf.InsertUnique([]byte("geeky ogre"))
+
+	// Lookup a string (and it a miss) if it exists in the cuckoofilter
+	log.Println(cf.Lookup([]byte("hello")))
+	log.Println(cf.Lookup([]byte("geeky ogre")))
+
+	count := cf.Count()
+	log.Println(count) // count == 1
+
+	// Delete a string (and it a miss)
+	cf.Delete([]byte("hello"))
+
+	count = cf.Count()
+	log.Println(count) // count == 1
+
+	// Delete a string (a hit)
+	cf.Delete([]byte("geeky ogre"))
+
+	count = cf.Count()
+	log.Println(count) // count == 0
+
+	cf.Reset() // reset
+
+	// 数据结构
 }
