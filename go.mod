d4469c80d514b16bcf3b2da939c500380b556898
张志国 <93757453@qq.com>
2023-05-22 16:52:37 +0800

    feat: add 常用

--- go.mod
+++ go.mod
@@ -5,14 +5,39 @@ go 1.18
 require github.com/google/uuid v1.3.0
 
 require (
+	github.com/RoaringBitmap/roaring v1.2.3 // indirect
 	github.com/bits-and-blooms/bitset v1.7.0 // indirect
 	github.com/bits-and-blooms/bloom/v3 v3.3.1 // indirect
+	github.com/blevesearch/bleve/v2 v2.3.7 // indirect
+	github.com/blevesearch/bleve_index_api v1.0.5 // indirect
+	github.com/blevesearch/geo v0.1.17 // indirect
+	github.com/blevesearch/go-porterstemmer v1.0.3 // indirect
+	github.com/blevesearch/gtreap v0.1.1 // indirect
+	github.com/blevesearch/mmap-go v1.0.4 // indirect
+	github.com/blevesearch/scorch_segment_api/v2 v2.1.4 // indirect
+	github.com/blevesearch/segment v0.9.1 // indirect
+	github.com/blevesearch/snowballstem v0.9.0 // indirect
+	github.com/blevesearch/upsidedown_store_api v1.0.2 // indirect
+	github.com/blevesearch/vellum v1.0.9 // indirect
+	github.com/blevesearch/zapx/v11 v11.3.7 // indirect
+	github.com/blevesearch/zapx/v12 v12.3.7 // indirect
+	github.com/blevesearch/zapx/v13 v13.3.7 // indirect
+	github.com/blevesearch/zapx/v14 v14.3.7 // indirect
+	github.com/blevesearch/zapx/v15 v15.3.9 // indirect
 	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang-module/carbon/v2 v2.2.3 // indirect
+	github.com/golang/geo v0.0.0-20230421003525-6adc56603217 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mschoch/smat v0.2.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/seiflotfy/cuckoofilter v0.0.0-20220411075957-e3b120b3f5fb // indirect
 	github.com/spf13/afero v1.9.3 // indirect
@@ -21,8 +46,10 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.15.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/sys v0.3.0 // indirect
+	go.etcd.io/bbolt v1.3.7 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"github.com/Tzzg/go-tool/data"
 	"github.com/Tzzg/go-tool/worker_pool"
 	"github.com/bits-and-blooms/bloom/v3"
+	"github.com/blevesearch/bleve/v2"
+	"github.com/emirpasic/gods/lists/arraylist"
+	"github.com/emirpasic/gods/utils"
 	"github.com/golang-module/carbon/v2"
 	cuckoo "github.com/seiflotfy/cuckoofilter"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 	"time"
 )
 
@@ -17,7 +21,7 @@ func main() {
 	a := data.StrToMd5("asdsa", 10)
 	log.Printf(a)
 
-	// test work pool
+	// 【work pool】
 	wp := worker_pool.NewWorkerPool(context.TODO(), 16, 10000)
 
 	add := wp.TryAdd(func(ctx context.Context) {
@@ -30,16 +34,16 @@ func main() {
 	log.Println("WaitAndClose")
 	wp.WaitAndClose()
 
-	// 时间处理
+	// 【时间处理】
 	//  go get -u github.com/golang-module/carbon/v2
 	log.Println(carbon.Now())
 
-	// config 配置
+	// 【config 配置】
 	// go get github.com/spf13/viper
 	viper.Set("KEY", "scsds")
 	log.Println(viper.GetString("KEY"))
 
-	// 布隆过滤器
+	// 【布隆过滤器】
 	// go get -u github.com/bits-and-blooms/bloom/v3
 	filter := bloom.NewWithEstimates(1000000, 0.01) // 构建能够接收 100 万个元素且误报率为 1% 的布隆过滤器
 	filter.Add([]byte("Love"))
@@ -72,5 +76,48 @@ func main() {
 
 	cf.Reset() // reset
 
-	// 数据结构
+	// 【数据结构】
+	// go get -u github.com/emirpasic/gods
+	list := arraylist.New()
+	list.Add("a")                     // ["a"]
+	list.Add("c", "b")                // ["a","c","b"]
+	list.Sort(utils.StringComparator) // ["a","b","c"]
+	a1, _ := list.Get(0)              // "a",true
+	log.Println(a1)
+	_, _ = list.Get(100)                  // nil,false
+	_ = list.Contains("a", "b", "c")      // true
+	_ = list.Contains("a", "b", "c", "d") // false
+	list.Swap(0, 1)                       // ["b","a",c"]
+	list.Remove(2)                        // ["b","a"]
+	list.Remove(1)                        // ["b"]
+	list.Remove(0)                        // []
+	list.Remove(0)                        // [] (ignored)
+	_ = list.Empty()                      // true
+	_ = list.Size()                       // 0
+	list.Add("a")                         // ["a"]
+	list.Clear()                          // []
+	list.Insert(0, "b")                   // ["b"]
+	list.Insert(0, "a")                   // ["a","b"]
+
+	// 文本分析
+	// go get -u github.com/blevesearch/bleve/v2
+	// open a new index
+	mapping := bleve.NewIndexMapping()
+	index, _ := bleve.New("example-bleve", mapping)
+
+	// index some data
+	_ = index.Index("2", "THIS IS SOME TEXT ok")
+
+	// search for some text
+	query := bleve.NewMatchQuery("text")
+	search := bleve.NewSearchRequest(query)
+	searchResults, _ := index.Search(search)
+	log.Println(searchResults)
+	index.Close()
+
+	// 删掉分析持久化数据
+	os.RemoveAll("example-bleve")
+
+	//
+
 }
